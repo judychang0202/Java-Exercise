5a5ba11e615fe24591e906b2da2a810c361f1dd9
01157105 <930711frank@gmail.com>
2024-05-08 16:42:38 +0800

    add comment

--- FirstClass3Team.java
+++ FirstClass3Team.java
@@ -2,6 +2,7 @@ public class FirstClass3Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157101\"\n姓名：\"張維麟\""); 
-        System.out.println("\"學號：\"01157148\"\n姓名：\"李宜蓁\""); 
+        System.out.println("\"學號：\"01157148\"\n姓名：\"李宜蓁\"");
+        System.out.println("\"學號：\"01157105\"\n姓名：\"楊子宏\"");
     }
 }
