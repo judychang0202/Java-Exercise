68ba522b0c3cf5cf561127c04d47cdfab271cc54
IamSB6 <01157148@mail.ntou.edu.tw>
2024-05-08 16:39:02 +0800

    Update FirstClass3Team.java

--- FirstClass3Team.java
+++ FirstClass3Team.java
@@ -2,5 +2,6 @@ public class FirstClass3Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157101\"\n姓名：\"張維麟\""); 
+        System.out.println("\"學號：\"01157148\"\n姓名：\"李宜蓁\""); 
     }
 }
